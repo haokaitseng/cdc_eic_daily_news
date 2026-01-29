7344808c283fe03c283047ccb931e9dfc3790c93
Hao Kai Tseng <kaitseng@cdc.gov.tw>
2026-01-29 14:02:11 +0800

    disease name mapping for colors, not fixed yet

--- utils/alert.py
+++ utils/alert.py
@@ -20,6 +20,41 @@ def normalize_date_series(s, colname=""):
     # 去掉時區、只保留日期
     return parsed.dt.tz_localize(None).dt.date
 
+ALERT_DISEASE_MAP = {
+    # COVID-19
+    "嚴重特殊傳染性肺炎": "COVID-19",
+    "新冠併發重症": "COVID-19",
+
+    # Influenza
+    "新型A型流感": "新型A型流感",
+    "禽類禽流感": "新型A型流感",
+
+    # Mpox
+    "M痘": "M痘",
+
+    # Polio
+    "小兒麻痺症": "小兒麻痺症",
+    "小兒麻痺症/急性無力肢體麻痺": "小兒麻痺症",
+
+    # Vector-borne / others already in color map
+    "登革熱": "登革熱",
+    "霍亂": "霍亂",
+    "屈公病": "屈公病",
+    "伊波拉病毒感染": "伊波拉病毒感染",
+    "茲卡病毒感染症": "茲卡病毒感染症",
+    "麻疹": "麻疹",
+
+    # Everything else → other
+    "馬堡病毒出血熱": "馬堡病毒出血熱",
+    "拉薩熱": "拉薩熱",
+    "黃熱病": "黃熱病",
+    "德國麻疹": "德國麻疹",
+    "白喉": "白喉",
+    "瘧疾": "瘧疾",
+    "中東呼吸症候群冠狀病毒感染症": "中東呼吸症候群冠狀病毒感染症",
+}
+
+
 
 def get_combined_travel_alerts(
     alert_history_path="data/TCDCTravelAlert_history.csv",
@@ -64,5 +99,12 @@ def get_combined_travel_alerts(
     # === 5. Final sanity check ===
     #print(" Combined rows:", len(df_all))
     #print(" effective NaT ratio:", df_all["effective"].isna().mean())
+
+    # 6. disease name mapping
+    df_all["alert_disease"] = (
+        df_all["alert_disease"]
+        .map(ALERT_DISEASE_MAP)
+        .fillna("其他疾病")
+    )
 
     return df_all
